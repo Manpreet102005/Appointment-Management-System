0e332ba5becb7d290483fe7ae05ef8b1400990c2
Manpreet102005 <singhmanpreet102005@gmail.com>
2026-02-19 02:09:46 +0530

    appointment repo fixes in parameters

--- src/repositries/AppointmentRepository.java
+++ src/repositries/AppointmentRepository.java
@@ -2,6 +2,9 @@ package repositries;
 
 import entities.Appointment;
 
+import java.time.LocalDateTime;
+
 public interface AppointmentRepository {
-    public void addAppointment(Appointment appointment);
+    public void addAppointment(int doctorId, LocalDateTime dateTime, int patientId, String patient);
+    public boolean cancelAppointment(Appointment appointment);
 }
