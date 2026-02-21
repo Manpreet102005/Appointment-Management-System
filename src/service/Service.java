9f92a4664301c416f5bc78468192b7e651e9925c
Manpreet102005 <singhmanpreet102005@gmail.com>
2026-02-22 00:41:07 +0530

    Service Layer

--- src/service/Service.java
+++ src/service/Service.java
@@ -2,17 +2,18 @@ package service;
 
 import entities.Appointment;
 import entities.Person;
-import repositries.AppointmentRepository;
-import repositries.DoctorRepository;
-import repositries.UserRepository;
+import entities.User;
+import repositries.*;
 import validations.AppointmentValidation;
 import validations.PersonValidation;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Service {
     private final AppointmentRepository appointmentRepository;
     private final DoctorRepository doctorRepository;
+    private final UserRepository userRepository;
     private final AppointmentValidation appointmentValidation;
     private final PersonValidation personValidation;
 
@@ -23,9 +24,11 @@ public class Service {
                    PersonValidation personValidation){
         this.appointmentRepository=appointmentRepository;
         this.doctorRepository=doctorRepository;
+        this.userRepository=userRepository;
         this.appointmentValidation=appointmentValidation;
         this.personValidation=personValidation;
     }
+
     public Appointment.Status addAppointment(int doctorId, LocalDateTime dateTime, int patientId, String patient){
         personValidation.validate(new Person(patientId,patient));
         appointmentValidation.validate(new Appointment(doctorId,patientId,patient,dateTime));
@@ -33,10 +36,12 @@ public class Service {
         if(doctorRepository.getDoctor(doctorId)==null){
             throw new RuntimeException("Doctor Not Found");
         }
+
         if(dateTime.isAfter(LocalDateTime.of(dateTime.getYear(),dateTime.getMonth(),dateTime.getDayOfMonth(),20,30))
         && dateTime.isBefore(LocalDateTime.of(dateTime.getYear(),dateTime.getMonth(),dateTime.getDayOfMonth(),10,0))){
             throw new RuntimeException("Working Hours: 10:00 am to 8:30 pm");
         }
+
         if(!appointmentRepository.isSlotAvailable(doctorId,dateTime)){
             throw new RuntimeException("Slot already booked");
         }
@@ -45,5 +50,15 @@ public class Service {
 
         appointmentRepository.addAppointment(appointment);
         return appointment.getStatus();
+    }
+
+    public boolean cancelAppointment(int doctorId, int patientId, LocalDateTime dateTime) {
+        if(dateTime.isBefore(LocalDateTime.now())){
+            throw new RuntimeException("Appointments can not be cancelled in past.");
+        }
+        String patientName=userRepository.getUser(patientId).getFullName();
+        Appointment appointment=new Appointment(doctorId,patientId,patientName,dateTime);
+        boolean flag=appointmentRepository.cancelAppointment(appointment);
+        return flag;
     }
 }
