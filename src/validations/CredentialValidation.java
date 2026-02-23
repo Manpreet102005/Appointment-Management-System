b10997ef7eedd9b9d7332b8a4841f0d02519b0ec
Manpreet102005 <singhmanpreet102005@gmail.com>
2026-02-24 00:21:30 +0530

    Add Credential validation

---
+++ src/validations/CredentialValidation.java
@@ -0,0 +1,31 @@
+package validations;
+
+import java.util.ArrayList;
+
+public class CredentialValidation {
+    static ArrayList<Character> allowedCharacters=new ArrayList<>();
+    private static void addingAllowedCharacters() {
+        for (char c = 'a'; c <= 'z'; c++) {
+            allowedCharacters.add(c);
+        }
+        for(int i=0;i<=9;i++){
+            allowedCharacters.add(Character.forDigit(i,10));
+        }
+        allowedCharacters.add('@');
+    }
+    public CredentialValidation(String username, String password){
+        addingAllowedCharacters();
+        if(username.length()<5 || username.length()>12){
+            throw new RuntimeException("Username length should be between 5 and 12.");
+        }
+        for(char c:username.toCharArray()){
+            if(!allowedCharacters.contains(c)){
+                throw new RuntimeException("allowed characters for username: a~z, 0~9 and @");
+            }
+        }
+        if(password.length()<8){
+            throw new RuntimeException("Password length should be atleast 8");
+        }
+    }
+
+}
