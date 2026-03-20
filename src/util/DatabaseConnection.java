effb518d2ad6b86cd506df51a7491779911be391
Manpreet102005 <singhmanpreet102005@gmail.com>
2026-03-20 23:12:34 +0530

    fixed: env

--- src/util/DatabaseConnection.java
+++ src/util/DatabaseConnection.java
@@ -1,21 +1,31 @@
 package util;
 
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Scanner;
+import java.util.Properties;
 
 public class DatabaseConnection {
     public static Connection getConnection() {
-        Scanner sc=new Scanner(System.in);
-        String url = "jdbc:mysql://Manpreet:3306/appointment_db";
-        String user = sc.nextLine();
-        String password = sc.nextLine();
         try {
+            Properties properties = new Properties();
+            FileInputStream inputStream = new FileInputStream("env.properties");
+            properties.load(inputStream);
+
+            String url = properties.getProperty("url");
+            String user = properties.getProperty("user");
+            String password = properties.getProperty("password");
             return DriverManager.getConnection(url, user, password);
         } catch (SQLException e) {
+            System.out.println("State: " + e.getSQLState());
+            System.out.println("Error Code: " + e.getErrorCode());
+            System.out.println("Message: " + e.getMessage());
+            return null;
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
+}
 }
