1b64ed9df6aab5330f2586aa7ec6ad271000b35f
Manpreet102005 <singhmanpreet102005@gmail.com>
2026-03-20 11:42:27 +0530

    handled already created  tables

--- database/schema.sql
+++ database/schema.sql
@@ -1,22 +1,22 @@
 CREATE DATABASE IF NOT EXISTS appointment_db;
 use appointment_db;
 
-CREATE TABLE person (
+CREATE TABLE  IF NOT EXISTS person (
        id int primary key ,
        fullname varchar(20) not null
 );
 
-CREATE TABLE patient(
+CREATE TABLE IF NOT EXISTS patient(
        id int primary key,
        fullname varchar(20) not null
 );
 
-CREATE TABLE doctor(
+CREATE TABLE IF NOT EXISTS doctor(
                        doctor_id int primary key ,
                        specialisation varchar(20)
 );
 
-CREATE TABLE appointment(
+CREATE TABLE IF NOT EXISTS appointment(
         appointment_id int primary key auto_increment,
         doctor_id int ,
         patient_id int not null ,
@@ -26,7 +26,7 @@ CREATE TABLE appointment(
         foreign key (doctor_id) references doctor(doctor_id) on delete set null
 );
 
-CREATE Table authorised_users(
+CREATE TABLE IF NOT EXISTS authorised_users(
         username varchar(20) primary key ,
         password varchar(20)
 );
