24679d238d433f711552cb6730a5a0611ba57712
Manpreet102005 <singhmanpreet102005@gmail.com>
2026-03-21 00:04:46 +0530

    Add: DB doctors repo

---
+++ src/repositries/impl/DBDoctorRepository.java
@@ -0,0 +1,74 @@
+package repositries.impl;
+
+import entities.Doctor;
+import repositries.DoctorRepository;
+import util.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBDoctorRepository implements DoctorRepository {
+    public void addDoctor(Doctor doctor){
+        String query="INSERT INTO doctors (id, fullname,specialisation) VALUES (?,?,?)";
+        try(Connection conn= DatabaseConnection.getConnection();
+            PreparedStatement ps= conn.prepareStatement(query)){
+            ps.setInt(1,doctor.getId());
+            ps.setString(2,doctor.getFullName());
+            ps.setString(3,doctor.getSpecialization());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("State: " + e.getSQLState());
+            System.out.println("Code : " + e.getErrorCode());
+            System.out.println("Msg  : " + e.getMessage());
+        }
+    }
+    public void removeDoctor(int id){
+
+    }
+    public Doctor getDoctor(int id){
+        String query = "SELECT * FROM doctors WHERE id=?";
+        Doctor doctor = null;
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(query)) {
+            ps.setInt(1,id);
+            ResultSet rs = ps.executeQuery(query);
+            if (rs.next()) {
+                doctor = new Doctor(rs.getInt("id"),
+                        rs.getString("fullname"),
+                        rs.getString("specialisation")
+                );
+            }
+        } catch (SQLException e) {
+            System.out.println("State: " + e.getSQLState());
+            System.out.println("Code : " + e.getErrorCode());
+            System.out.println("Msg  : " + e.getMessage());
+        }
+        return doctor;
+    }
+    public List<Doctor> getAllDoctors(){
+        List<Doctor> list= new ArrayList<>();
+        String query = "SELECT * FROM doctors";
+        Doctor doctor = null;
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(query)) {
+
+            ResultSet rs = ps.executeQuery(query);
+            while (rs.next()) {
+                doctor = new Doctor(rs.getInt("id"),
+                        rs.getString("fullname"),
+                        rs.getString("specialisation")
+                );
+                list.add(doctor);
+            }
+        } catch (SQLException e) {
+            System.out.println("State: " + e.getSQLState());
+            System.out.println("Code : " + e.getErrorCode());
+            System.out.println("Msg  : " + e.getMessage());
+        }
+        return list;
+    }
+}
